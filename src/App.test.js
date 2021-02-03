4b89ff6ed212f9db52d2193c4fabfb484036f0cd
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-02-03 15:48:37 +0000

    back

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,6 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-// test('Shows Hello, world!', () => {
-//   render(<App />)
-
-//   expect(screen.getByText('Hello, World!')).toBeInTheDocument()
-// })
-
 test("Shows hello world", () => {
   render(<App />);
 
--- src/NoteList.js
+++ src/NoteList.js
@@ -5,22 +5,43 @@ class NoteList extends Component {
     super(props);
     this.state = {
       note: "",
+      notesArray: [],
     };
   }
   addNote = (e) => {
     this.setState({ note: e.target.value });
+
   };
+
+  displayNote = (e) => {
+    let n = this.state.note
+    let array = this.state.notesArray
+    array.push(n)
+    this.setState({noteArray: array})
+    // this.setState({finalNote: this.state.note});
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="NoteList">
         <form>
           <label>
             Name:
-            <input type="text" name="name" onChange={this.addNote} />
+            <input type="text" name="name" onChange={this.addNote}/>
           </label>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" onClick={this.displayNote}  />
         </form>
-        {this.state.note}
+        <div className='parent'>
+        <ul>
+        {this.state.notesArray.map(function (note) {
+            return (
+              
+              <li>{note}</li>
+            )
+        })}
+        </ul>
+        </div>
       </div>
     );
   }
